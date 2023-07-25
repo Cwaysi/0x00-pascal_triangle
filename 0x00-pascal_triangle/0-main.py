98d9e4974d1a505b6e410546d96c932f8ed2ec4c
Justice Morison Gurah <cwaysimorison@outlook.com>
2023-07-25 18:00:40 +0000

    Update 0-main.py

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-0-main to execute
+0-main
 """
 pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
-
 def print_triangle(triangle):
     """
-    Print the pascal triangle from the pascal file
+    Print the triangle
     """
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
