5860329a7284f7d2281b90f380ab65b91aa9348f
Justice Morison Gurah <cwaysimorison@outlook.com>
2023-07-25 17:52:44 +0000

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,11 +2,20 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    pascal = [[1]]  # The first row is always [1]
 
     for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1] + [prev_row[j] + prev_row[j + 1] for j in range(len(prev_row) - 1)] + [1]
-        triangle.append(new_row)
+        row = [1]  # The first element of each row is always 1
+        for j in range(1, i):
+            row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
+        row.append(1)  # The last element of each row is always 1
+        pascal.append(row)
 
-    return triangle
+    return pascal
+
+# Test the function
+# n = 5
+# result = pascal_triangle(n)
+# for row in result:
+#    print(row)
+
